61994a1ee4805bcbfecb1e4254d7f111d7393ae5
tanzeel-khan1 <tanzeel0680@gmail.com>
2026-06-04 11:45:15 +0500

    fixess time

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { createRequire } from 'module';
 
-const require = createRequire(import.meta.url);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -14,4 +12,4 @@ export default defineConfig({
       '/api': 'http://10.69.5.70:5000',
     },
   },
-}); 
+});
